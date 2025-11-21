8b84ca80111cbe60c75a6c2b9ac0c5ab11c969e4
Grayson Moore <moor8456@hotmail.com>
2025-11-21 08:50:46 -0800

    Update cost calculation in button click handler
    Refactor cost calculation logic for clarity and accuracy.

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 $("#button").click(function(event) {
-	var a = Number($("#inputh").val());
+	var hours = Number($("#inputh").val());
 	var b = Number($("#inputm").val());
-	var answer = a * 170 + b * 3.35;
+	var matcost = 0;
+	if (b < 20) {
+		matcost = b * 5;
+	} else if (b >= 20 && b <= 500) {
+		matcost = -0.007083333 * b + 5.14166667;
+	} else if (b > 500) {
+		matcost = b * 1.6;
+	}
+	var answer = hours * 170 + matcost;
 	$("#answer h1").detach();
 	$("p").detach();
 	$("#answer").append("<p>Standard Price: </p><h1>$" + answer.toFixed(2) + "</h1>");
-	var answer2 = answer * 1.75;
+	/*var answer2 = answer * 1.75;
 	$("#answer2 h1").detach();
-	$("#answer2").append("<p>After Hours Price: </p><h1>$" + answer2.toFixed(2) + "</h1>");
+	$("#answer2").append("<p>After Hours Price: </p><h1>$" + answer2.toFixed(2) + "</h1>");*/
 });
